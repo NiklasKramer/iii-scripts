b050f925740a8d2188611633aa85754b77dde653
Niklas Kramer <Niklas.Kramer@me.com>
2025-04-19 22:08:02 +0200

    recieve midi

--- enc.lua
+++ enc.lua
@@ -1,4 +1,7 @@
 local screen_index = 1
+
+arc_sensitivity = 3
+
 local cc_map = {
     { 100, 101, 102, 103 },
     { 104, 105, 106, 107 },
@@ -23,10 +26,20 @@ function arc(n, d)
     midi_cc(cc_map[screen_index][n], values[screen_index][n], 1)
 end
 
+function midi_rx(ch, status, data1, data2)
+    if status == 176 then -- CC message
+        for i = 1, 4 do
+            if data1 == cc_map[screen_index][i] then
+                values[screen_index][i] = clamp(data2, 0, 127)
+            end
+        end
+    end
+end
+
 function arc_redraw(n)
     arc_led_all(n, 0)
-    local start_led = 48 -- 8 o'clock
-    local end_led = 17   -- 4 o'clock
+    local start_led = 44
+    local end_led = 21
     local led_span = ((end_led - start_led + 64) % 64)
     local led_pos = math.floor(values[screen_index][n] / 127 * led_span)
     for i = 0, led_pos do
@@ -38,8 +51,8 @@ function arc_redraw(n)
     arc_refresh()
 
     if n == 1 then
-        local base = 26
-        local spacing = 4
+        local base = 28
+        local spacing = 3
         for i = 0, 3 do
             local led = (base + (3 - i) * spacing) % 64
             arc_led(n, led, (i + 1) == screen_index and 10 or 2)
@@ -52,4 +65,8 @@ function arc_key(k, z)
         screen_index = (screen_index % 4) + 1
         print('')
     end
+end
+
+for i = 1, 4 do
+    arc_res(i, arc_sensitivity)
 end
